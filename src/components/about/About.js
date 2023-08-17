8ff8c9709b45c94d426afa66fb2c81ebab0ffdd1
Ariel <mt.calderon.alejandro@gmail.com>
2023-08-17 16:01:46 -0500

    Layout  #about

---
+++ src/components/about/About.js
@@ -0,0 +1,16 @@
+import Data from "./Data";
+import "./about.css";
+
+function About() {
+  return (
+    <div className="about section">
+      <div className="about__container container">
+        <h1 className="about__title">About me</h1>
+        <p className="about__subtitle">My introduccion</p>
+        <Data />
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/about/Data.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Data() {
+  return (
+    <div className="about__data">
+      <div className="about__img"></div>{" "}
+      <div className="about__description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero qui
+          sed illo ex totam, quis quo aut laudantium praesentium doloribus
+          maxime! Natus, culpa officia! Porro, architecto asperiores? Eligendi,
+          obcaecati ad! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Rerum expedita voluptatem, dolores nemo, officia autem sed, ullam quo
+          aperiam labore tenetur earum. Eligendi nesciunt ptiae quo in
+          voluptatem? Ab animi debitis deserunt eum beatae!
+        </p>
+        <button className="button"> Download CV</button>
+      </div>
+    </div>
+  );
+}
+
+export default Data;
