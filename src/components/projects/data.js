4bdefd13726d45e7ad3fb137c615899381c81b10
Ariel <mt.calderon.alejandro@gmail.com>
2023-08-20 05:58:17 -0500

    [data] - Ecommerce react

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -1,8 +1,9 @@
 const projects = [
   {
     id: 1,
-    image: "http://unsplash.it/400",
-    title: `Project 1`,
+    image:
+      "https://lh3.googleusercontent.com/pw/AIL4fc9b5fcz4w4Ms1OZPT_ZtKI6yMYC2iKkM-yPgJyIULOjrB1uBsPgB-aUa5POE5hv4HTL4tNNC-yuGVL-q5atrwZ7GXVFyAkFCdblMMdxfGpIotzT3_I=w2400",
+    title: `E-commerce React`,
     live: "bla",
     repo: "bla",
   },
