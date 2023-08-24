599a55227b4aad932dc8421497889a3a4bd16a8a
Ariel <mt.calderon.alejandro@gmail.com>
2023-08-24 12:28:35 -0500

    Edit about info

--- src/components/about/Data.js
+++ src/components/about/Data.js
@@ -6,15 +6,26 @@ function Data() {
       <div className="about__img"></div>{" "}
       <div className="about__description">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero qui
-          sed illo ex totam, quis quo aut laudantium praesentium doloribus
-          maxime! Natus, culpa officia! Porro, architecto asperiores? Eligendi,
-          obcaecati ad! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Rerum expedita voluptatem, dolores nemo, officia autem sed, ullam quo
-          aperiam labore tenetur earum. Eligendi nesciunt ptiae quo in
-          voluptatem? Ab animi debitis deserunt eum beatae!
+          Dedicated to everything related to web development in general. I have
+          experience using the fundamentals of the web along with React in the
+          creation of personal projects and small exercises. I seek for
+          implement best practices in every line of code I write, ensuring
+          quality in my work.
         </p>
-        <button className="about__button button"> Download CV</button>
+        <p>
+          Currently, I am looking for new job opportunities that will challenge
+          me and allow me grow professionally. My diligence and responsibility
+          are attributes that many companies would find attractive. I consider
+          myself a proactive person when it comes to facing all kinds of
+          problems and difficulties, learning to be more methodical when looking
+          for and trying out different solutions.
+        </p>
+        <p className="mb-4">
+          Technology is my path to the future, and my goal is to immerse myself
+          in a competitive to immerse myself in a competitive environment that
+          stimulates my continuous growth.
+        </p>
+        <button className="about__button button "> Download CV</button>
       </div>
     </div>
   );
