6b01bfdfcc0f80b38b5bdd7ddd62571b23074a39
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-01 06:50:49 -0500

    [--]

--- src/components/home/Data.js
+++ src/components/home/Data.js
@@ -4,7 +4,7 @@ function Data() {
   return (
     <div className="home__data">
       <h1 className="home__title"> Alejandro</h1>
-      <h3 className="home__subtitle">Frontend Developer</h3>
+      <h3 className="home__subtitle">Software Developer</h3>
       <p className="home__description">
         Hi! I'm a passionate software developer and I'm excited about the
         opportunity to join and contribute a dynamic team in the technology
