646e8d212355db2ec0e2df06c841d921eae2e1f4
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-01 06:51:04 -0500

    Rework: projects

--- src/components/projects/Works.js
+++ src/components/projects/Works.js
@@ -1,23 +1,37 @@
 import React from "react";
 import { projects } from "./data";
-import { FaArrowAltCircleRight, FaGithub } from "react-icons/fa";
+import {
+  FaArrowAltCircleRight,
+  FaGithub,
+  FaLocationArrow,
+  FaLongArrowAltUp,
+  FaShoppingCart,
+} from "react-icons/fa";
 
 function Works() {
   console.log(projects);
   return (
-    <div className="projects__container container grid">
+    <div className="projects__container container">
       {projects.map((project) => (
-        <div key={project.id} className="project__card">
-          <img src={project.image} alt="" className="project__img" />
-          <div className="projects__bottom">
-            <p className="project__title">{project.title}</p>
-            <a className="projects__bottom--item" href={project.repo}>
-              <FaGithub />
-            </a>
-            <a className="projects__bottom--item" href={project.live}>
-              <FaArrowAltCircleRight />
-            </a>
+        <div className="project__card">
+          <div className="project__information">
+            <p className="project__years">{project.years}</p>
+            <h1 className="project__title">
+              {project.title}{" "}
+              <span className="project__icon">
+                <FaLocationArrow>
+                  <a href={project.live}>live</a>
+                </FaLocationArrow>
+              </span>{" "}
+            </h1>
+            <p className="project__description">{project.description}</p>
+            <div className="project__technologies">
+              {project.technologies.map((item) => (
+                <p className="project__technologies--item">{item}</p>
+              ))}
+            </div>
           </div>
+          <div className="project__image">{project.image}</div>
         </div>
       ))}
     </div>
--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -1,31 +1,58 @@
+import {
+  FaBlog,
+  FaBlogger,
+  FaIdeal,
+  FaMicroblog,
+  FaShoppingCart,
+  FaStickyNote,
+} from "react-icons/fa";
+
+const techDomain = {
+  react: "React",
+  tailwind: "Tailwind",
+  next: "Next",
+  express: "Express",
+  mongo: "Mongo",
+};
+
 const projects = [
   {
     id: 1,
-    image:
-      "https://lh3.googleusercontent.com/pw/AIL4fc9b5fcz4w4Ms1OZPT_ZtKI6yMYC2iKkM-yPgJyIULOjrB1uBsPgB-aUa5POE5hv4HTL4tNNC-yuGVL-q5atrwZ7GXVFyAkFCdblMMdxfGpIotzT3_I=w2400",
-    title: `E-commerce React`,
+    image: <FaShoppingCart />,
+    title: `Fullstack e-commerce * Tech Shop`,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, ad. Velit laboriosam laborum beatae voluptates sapiente explicabo reiciendis corrupti iure fugiat eum, debitis tempora voluptatem. Pariatur quibusdam facere assumenda magnam.",
+    technologies: [
+      techDomain.react,
+      techDomain.tailwind,
+      techDomain.next,
+      techDomain.express,
+      techDomain.mongo,
+    ],
     live: "https://ecommerce-logo.netlify.app/",
+    years: "2023",
     repo: "https://github.com/mt-alejo/project-ecommerce-react",
   },
   {
     id: 2,
-    image: "http://unsplash.it/399",
-    title: `Project 2`,
+    image: <FaStickyNote />,
+    title: `Fullstack blog app * Habits Blog`,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, ad. Velit laboriosam laborum beatae voluptates sapiente explicabo reiciendis corrupti iure fugiat eum, debitis tempora voluptatem. Pariatur quibusdam facere assumenda magnam.",
+    technologies: [],
     live: "bla",
+    years: "2023",
     repo: "bla",
   },
   {
     id: 3,
-    image: "http://unsplash.it/398",
+    image: <FaShoppingCart />,
     title: `Project 3`,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, ad. Velit laboriosam laborum beatae voluptates sapiente explicabo reiciendis corrupti iure fugiat eum, debitis tempora voluptatem. Pariatur quibusdam facere assumenda magnam.",
+    technologies: [],
     live: "bla",
-    repo: "bla",
-  },
-  {
-    id: 4,
-    image: "http://unsplash.it/397",
-    title: `Project 4`,
-    live: "bla",
+    years: "2023",
     repo: "bla",
   },
 ];
