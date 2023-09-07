37c1ce1e59195fd1fa93f0ca16b6381286cab686
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-07 18:27:23 -0500

    Links

--- src/components/home/Social.js
+++ src/components/home/Social.js
@@ -4,13 +4,25 @@ import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 function Social() {
   return (
     <div className="home__social">
-      <a className="home__social-item" href="" target="_blank">
+      <a
+        className="home__social-item"
+        href="https://github.com/mt-alejo"
+        target="_blank"
+      >
         <FaGithub />
       </a>
-      <a className="home__social-item" href="" target="_blank">
+      <a
+        className="home__social-item"
+        href="https://www.linkedin.com/in/ariel-alejandro-calderon"
+        target="_blank"
+      >
         <FaLinkedin />
       </a>
-      <a className="home__social-item" href="" target="_blank">
+      <a
+        className="home__social-item"
+        href="https://twitter.com/mt_alejo_"
+        target="_blank"
+      >
         <FaTwitter />
       </a>
     </div>
