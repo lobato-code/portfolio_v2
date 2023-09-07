1f543065c80726f31295f1a8d8a0f26d3e876219
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-09-07 17:22:45 -0500

    Edit projects info

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -1,6 +1,9 @@
-import { FaShoppingCart, FaStickyNote } from "react-icons/fa";
+import { FaChartBar, FaShoppingCart, FaStickyNote } from "react-icons/fa";
 
 const techDomain = {
+  html: "HTML",
+  css: "CSS",
+  js: "JS",
   react: "React",
   tailwind: "Tailwind",
   next: "Next",
@@ -12,35 +15,47 @@ const projects = [
   {
     id: 1,
     image: <FaShoppingCart />,
-    title: "Frontend e-commerce • Tech Shop",
+    title: `React e-commerce •-• Tech Shop`,
     description:
-      "View products, view description, sort by categories, pick orders, make checkouts, see your orders.",
-    technologies: [techDomain.react, techDomain.tailwind],
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, ad. Velit laboriosam laborum beatae voluptates sapiente explicabo reiciendis corrupti iure fugiat eum, debitis tempora voluptatem. Pariatur quibusdam facere assumenda magnam.",
+    technologies: [
+      techDomain.html,
+      techDomain.css,
+      techDomain.js,
+      techDomain.tailwind,
+      techDomain.react,
+    ],
     live: "https://ecommerce-logo.netlify.app/",
     years: "2023",
     repo: "https://github.com/mt-alejo/project-ecommerce-react",
   },
   {
     id: 2,
-    image: <FaStickyNote />,
-    title: `Fullstack blog app • Habits Blog`,
+    image: <FaChartBar />,
+    title: `Next dashboard •-• Shop Administrator`,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, ad. Velit laboriosam laborum beatae voluptates sapiente explicabo reiciendis corrupti iure fugiat eum, debitis tempora voluptatem. Pariatur quibusdam facere assumenda magnam.",
-    technologies: [],
-    live: "bla",
+    technologies: [
+      techDomain.html,
+      techDomain.css,
+      techDomain.js,
+      techDomain.tailwind,
+      techDomain.next,
+    ],
+    live: "https://practice-next-dashboard.vercel.app/",
     years: "2023",
-    repo: "bla",
+    repo: "https://github.com/mt-alejo/project-dashboard-next",
   },
   {
     id: 3,
-    image: <FaShoppingCart />,
-    title: `Project 3`,
+    image: <FaStickyNote />,
+    title: `Fullstack blog app •-• Habits Blog (coming soon..)`,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, ad. Velit laboriosam laborum beatae voluptates sapiente explicabo reiciendis corrupti iure fugiat eum, debitis tempora voluptatem. Pariatur quibusdam facere assumenda magnam.",
-    technologies: [],
-    live: "bla",
+    technologies: ["Typescript", "Sass"],
+    live: "#",
     years: "2023",
-    repo: "bla",
+    repo: "#",
   },
 ];
 
